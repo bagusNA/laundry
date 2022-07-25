6bd1065dde2ed9b16c966140c617f119bf37bf90
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2022-07-25 21:21:04 +0800

    Initial commit

---
+++ resources/js/Pages/Home/Index.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Navbar from '@/Components/Navbar.vue';
+
+</script>
+
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import '../css/app.css';
+
+InertiaProgress.init();
+
+createInertiaApp({
+  resolve: name => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+  setup({el, App, props, plugin}) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el);
+  }
+});
