d1bc7c48c77b873f60319b1d4c73ac842e2cf785
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2022-07-26 14:16:03 +0800

    Add bootstrap

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,11 +5,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Laundry App</title>
+
+  @vite('node_modules/bootstrap/dist/css/bootstrap.min.css')
   {{-- Inertia --}}
   @vite('resources/js/app.js')
   @inertiaHead
 </head>
 <body>
   @inertia
+  @vite('node_modules/bootstrap/dist/js/bootstrap.min.js')
 </body>
 </html>
