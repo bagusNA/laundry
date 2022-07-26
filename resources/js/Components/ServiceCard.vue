f78e5b23390ae76978d0adb58fb0ffb9e20c9dd4
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2022-07-26 20:53:50 +0800

    Feat: services list

---
+++ resources/js/Components/ServiceCard.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+
+defineProps(['name', 'price', 'image', 'icon', 'onClick']);
+</script>
+
+<template>
+  <div 
+    class="service bg-primary rounded-3"
+    @click="onClick"
+  >
+    <div class="d-flex flex-column justify-content-center align-items-center text-light rounded-top p-3">
+      <img v-if="image"
+        :src="image" 
+        alt="Service Logo"
+      >
+      <Icon v-else-if="icon" 
+        :icon="icon"
+        class="service__icon"
+      />
+      <span>{{ name }}</span>
+    </div>
+    <div class="service__price bg-secondary text-light rounded-bottom">Rp. {{ price }} / kg</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.service {
+  transition: 100ms;
+
+  &__icon {
+    font-size: 2rem;
+    margin: 1rem 0;
+  }
+
+  &__price {
+    padding: 0.5rem 0;
+    text-align: center;
+  }
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.9;
+  }
+}
+</style>
--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -2,6 +2,11 @@
 import Navbar from '@/Components/Navbar.vue';
 import Category from '@/Components/Category.vue';
 import UserCard from '@/Components/UserCard.vue';
+import ServiceCard from '@/Components/ServiceCard.vue';
+
+function hi() {
+  console.log('hi')
+}
 
 </script>
 
@@ -10,8 +15,16 @@ import UserCard from '@/Components/UserCard.vue';
     <Navbar class="sidebar"/>
     <main class="main">
       <h2 class="title">Daftar Layanan</h2>
-      <div class="kategori">
+      <div class="category-list">
         <Category v-for="n in 5" />
+      </div>
+      <div class="service-list">
+        <ServiceCard v-for="n in 10"
+          name="Reguler 3 Hari" 
+          :price="5000"
+          icon="ion:home"
+          :onClick="hi"
+        />
       </div>
     </main>
     <aside class="side-content">
@@ -34,23 +47,36 @@ import UserCard from '@/Components/UserCard.vue';
     padding-bottom: 1.5rem;
   }
 
-  .kategori {
-    // display: grid;
-    // grid-template-columns: repeat(1, 100px);
+  .category-list {
     display: flex;
     gap: 0 10px;
+    padding-bottom: 1.5rem;
+  }
+
+  .service-list {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+    justify-content: space-between;
+    gap: 10px;
   }
 }
 
 .side-content {
   width: 30%;
   padding: 0.5rem;
-  background-color: salmon;
 }
 
 @media only screen and (min-width: 768px) {
   .sidebar {
     min-width: 6rem;
+  }
+  
+  .main {
+    padding: 1.5rem 2rem;
+
+    .service-list {
+      grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    }
   }
 }
 </style>
