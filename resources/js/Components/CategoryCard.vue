3739e9c1321162d95e065d4560646d094ce9fc25
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2022-07-26 20:54:32 +0800

    Category card hover effect

--- resources/js/Components/CategoryCard.vue
+++ resources/js/Components/CategoryCard.vue
@@ -4,27 +4,34 @@ import { Icon } from '@iconify/vue';
 
 <template>
   <a 
-    class="category__item p-2 rounded bg-secondary text-light bg-opacity-75 text-center text-decoration-none"
+    class="category rounded bg-secondary text-light bg-opacity-75 text-center text-decoration-none"
     href="#"
   >
-    <span class="category__item__logo">
+    <span class="category__logo">
       <Icon icon="ion:home" />
     </span>
-    <p class="category__item__title">Category</p>
+    <p class="category__title">Category</p>
   </a>
 </template>
 
 <style scoped lang="scss">
-.category__item {
-  font-size: 0.8rem;
+.category {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
+  font-size: 1rem;
+  padding: 1rem;
+  transition: 100ms;
 
   &__logo {
-    font-size: 1.5rem;
+    font-size: 2rem;
     padding-bottom: 1rem;
+  }
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.9;
   }
 }
 </style>
