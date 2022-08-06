5ec921a137f7834eb009056424a0b02475fa050b
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2022-08-06 09:41:15 +0800

    Misc: use layout for transaksi page

--- resources/js/Pages/Transaksi/Index.vue
+++ resources/js/Pages/Transaksi/Index.vue
@@ -1,20 +1,15 @@
 <script setup>
 import { currencyFormat } from '@/utils/currencyFormat.ts';
 import { dateFormat } from '@/utils/dateFormat.ts';
-
-import bgImage from '@/assets/img/bg-full.jpeg';
-import Navbar from '@/Components/Navbar.vue';
+import BaseLayout from '@/Components/layouts/BaseLayout.vue';
 
 defineProps(['semuaTransaksi']);
 </script>
 
 <template>
-  <div class="wrapper"
-    :style="{ backgroundImage: `url('${bgImage}')`}"
-  >
-    <Navbar class="sidebar" />
-    <div class="main">
-      <h2 class="title">Daftar Transaksi</h2>
+  <BaseLayout>
+    <template #title>Transaksi</template>
+    <template #content>
       <div class="p-2 rounded bg-primary text-light">
         <table class="table text-light">
           <thead>
@@ -42,39 +37,8 @@ defineProps(['semuaTransaksi']);
           </tbody>
         </table>
       </div>
-      
-      <!-- <div class="py-3">
-        <h2 class="title">Daftar Transaksi</h2>
-          <div class="p-2 rounded bg-primary text-light">
-            <table class="table text-light">
-              <thead>
-                <tr>
-                  <th scope="col">No.</th>
-                  <th scope="col">Nama Pelanggan</th>
-                  <th scope="col">Tanggal Pesan</th>
-                  <th scope="col">Total Harga</th>
-                  <th scope="col">Status</th>
-                  <th scope="col">Detail</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="(transaksi, index) in semuaTransaksi">
-                  <th scope="row">{{ semuaTransaksi.length - index }}</th>
-                  <td>{{ transaksi.pelanggan.nama }}</td>
-                  <td>{{ transaksi.created_at }}</td>
-                  <td>{{ currencyFormat(transaksi.total_harga) }}</td>
-                  <td>{{ transaksi.status_selesai ? 'Selesai' : 'Dalam Proses' }}</td>
-                  <td>
-                    <button type="button" class="btn btn-success">Selesai</button>
-                    <button type="button" class="btn btn-secondary">Detail</button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-      </div> -->
-    </div>
-  </div>
+    </template>
+  </BaseLayout>
 </template>
 
 <style scoped lang="scss">
